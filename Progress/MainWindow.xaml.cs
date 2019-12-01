c3499a1da9c52b896002ddac975bfc8322bb9466
esim7 <makishev@yahoo.com>
2019-12-02 02:48:25 +0600

    Добавил progressbar

---
+++ Progress/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Progress
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void load_Button(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = border.Background.Opacity;
+            animation.To = ActualWidth;
+            animation.Duration = TimeSpan.FromSeconds(7);
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+            border.BeginAnimation(Rectangle.WidthProperty, animation);
+
+
+
+
+        }
+    }
+}
